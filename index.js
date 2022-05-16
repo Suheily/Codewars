225550b56e4909b91a50c155a253e25d707b1e7e
Suheily <suheilyq@gmail.com>
2022-05-17 01:10:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // Level: 6 Kyu
 
+// Write Number in Expanded Form
+// Date: 5/17
+// Instructions: Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+  let expanded = num.toString().split("")
+
+  for(let i = 0; i < expanded.length; i++){
+    for(let j = expanded.length - i; j > 1; j--){
+      expanded[i] += "0"
+    }
+  }
+  expanded = expanded.filter(value => !value.startsWith(0))
+  return expanded.join(" + ")
+}
+
+
+// Level: 6 Kyu
+
 // Find the Parity Outlier
 // Date: 5/16
 // Instructions: You are given an array (which will have a length of at least 3, 
