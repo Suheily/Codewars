a4b62efef89091ceb83512f3848e3ba8d75bbc8a
Suheily <suheilyq@gmail.com>
2022-05-18 00:59:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 // Level: 6 Kyu
 
+// Does My Number Look Big in This?
+// Date: 5/18
+// Instructions: A Narcissistic Number is a positive number which is the sum of its own digits, 
+// each raised to the power of the number of digits in a given base. In this Kata, we will restrict 
+// ourselves to decimal (base 10).
+// For example, take 153 (3 digits), which is narcisstic:
+//  1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+//  1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number
+// is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero 
+// integers will be passed into the function.
+
+function narcissistic(value) {
+  let narcissus = value.toString().split("")
+  let pond = []
+  
+  for(let i = 0; i < narcissus.length; i++){
+    if(narcissus[i] === "1"){
+      pond.push(parseInt(narcissus[i]))
+    } else {
+    pond.push(parseInt(narcissus[i] ** narcissus.length))
+    }
+  }
+  
+  let echo = pond.reduce((a,b) => a + b, 0)
+  if( echo === value){
+      return true
+    } else {
+      return false
+    }
+}
+
+
+// Level: 6 Kyu
+
 // Write Number in Expanded Form
 // Date: 5/17
 // Instructions: Write Number in Expanded Form
