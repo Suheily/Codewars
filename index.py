fa949e5017dd6305f5210aa257542d02f5a29ce5
Suheily <suheilyq@gmail.com>
2022-05-20 00:00:33 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,28 @@
+# Level: 6 Kyu
+
+# Your Order, Please
+# Date: 5/20
+# Instructions: Your task is to sort a given string. Each word in the string will contain a single number.
+# This number is the position the word should have in the result.
+# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+# If the input string is empty, return an empty string. The words in the input String will only contain valid 
+# consecutive numbers.
+# Examples:
+# "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+# "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+# ""  -->  ""
+
+def order(sentence):
+    sentence = sentence.split()
+    new_sentence = []
+    
+    for i in range(1,10):
+        for j in list(sentence):
+            if str(i) in j:
+                new_sentence.append(j)
+    return " ".join(new_sentence)
+
+
 # Level: 7 Kyu
 
 # Binary Addition
