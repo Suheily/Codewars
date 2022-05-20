95db483ced2d2e99198d3720048f97aff5e3b84e
Suheily <suheilyq@gmail.com>
2022-05-21 00:30:49 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,28 @@
+#  Level: 5 Kyu
+
+#  RGB To Hex Conversion
+#  Date: 5/21
+#  Instructions: The rgb function is incomplete. Complete it so that passing in RGB decimal values will result
+#  in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that 
+#  fall out of that range must be rounded to the closest valid value.
+#  Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+#  The following are examples of expected output values:
+#  rgb(255, 255, 255) // returns FFFFFF
+#  rgb(255, 255, 300) // returns FFFFFF
+#  rgb(0,0,0) // returns 000000
+#  rgb(148, 0, 211) // returns 9400D3
+
+def rgb(r, g, b):
+    return "{:02X}{:02X}{:02X}".format(hex_convert(r), hex_convert(g), hex_convert(b))
+
+def hex_convert(value):
+    if value < 0:
+        return 0
+    if value > 255:
+        return 255
+    return value
+
+
 # Level: 6 Kyu
 
 # Your Order, Please
