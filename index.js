cadd1d3cc4c3608def6d4a633cee331b8780f513
Suheily <suheilyq@gmail.com>
2022-05-21 00:30:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+// Level: 5 Kyu
+
+// RGB To Hex Conversion
+// Date: 5/21
+// Instructions: The rgb function is incomplete. Complete it so that passing in RGB decimal values will result
+// in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that 
+// fall out of that range must be rounded to the closest valid value.
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+// The following are examples of expected output values:
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+function rgb(r, g, b){
+   function hexConvert(value){
+     if(value < 0){
+       return "00"
+     } else if (value > 255){
+       return "FF"
+     } else {
+       return ("0"+(Number(value).toString(16))).slice(-2).toUpperCase()
+     }
+  }
+  return hexConvert(r) + hexConvert(g) + hexConvert(b)
+}
+
+
 // Level: 6 Kyu
 
 // Your Order, Please
