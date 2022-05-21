0c567a951c228d334ed42a66aa5f7e936c987533
Suheily <suheilyq@gmail.com>
2022-05-22 01:55:29 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,26 @@
+#  Level: 6 Kyu
+
+#  Two Sum
+#  Date: 5/22
+#  Instructions: Write a function that takes an array of numbers (integers for the tests) and a target number.
+#  It should find two different items in the array that, when added together, give the target value. The indices 
+#  of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+#  For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+#  The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be 
+#  numbers; target will always be the sum of two different items from that array).
+#  Based on: http://oj.leetcode.com/problems/two-sum/
+#  twoSum [1, 2, 3] 4 === (0, 2)
+
+def two_sum(numbers, target):
+    hashmap = {}
+    
+    for i in range(0, len(numbers)):
+        if numbers[i] not in hashmap:
+            hashmap[target - numbers[i]] = i
+        else:
+            return [i, hashmap[numbers[i]]]
+
+
 #  Level: 5 Kyu
 
 #  RGB To Hex Conversion
@@ -112,10 +135,9 @@ def expanded_form(num):
 
 #  Find the Parity Outlier
 #  Date: 5/16
-#  Instructions: You are given an array (which will have a length of at least 3, 
-#  but could be very large) containing integers. The array is either entirely 
-#  comprised of odd integers or entirely comprised of even integers except for a 
-#  single integer N. Write a method that takes the array as an argument and returns 
+#  Instructions: You are given an array (which will have a length of at least 3, but could be very large) 
+#  containing integers. The array is either entirely comprised of odd integers or entirely comprised of even 
+#  integers except for a single integer N. Write a method that takes the array as an argument and returns 
 #  this "outlier" N.
 
 def find_outlier(integers):
