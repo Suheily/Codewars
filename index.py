c1e5d17d1bc24e64c6248a9e07f80c09d1033bf2
Suheily <suheilyq@gmail.com>
2022-05-24 23:14:33 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,18 @@
+#  Level: 5 Kyu
+
+#  Extract The Domain Name From A URL
+#  Date: 5/24
+#  Instructions:Write a function that when given a URL as a string, parses out just the domain name 
+#  and returns it as a string. For example:
+#  * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+#  * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+#  * url = "https://www.cnet.com"                -> domain name = "cnet"
+
+def domain_name(url):
+    domain = url.replace("https://","").replace("http://","").replace("www.","")
+    return domain.split(".")[0]
+
+
 #  Level: 4 Kyu
 
 #  Split Comments
