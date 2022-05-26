42bf6416cef12855c6c103fc9b4397a5fbd1827e
Suheily <suheilyq@gmail.com>
2022-05-26 23:44:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+//  Level: 8 Kyu
+
+//  Powers of 2
+//  Date: 5/26
+//  Instructions: Complete the function that takes a non-negative integer n as input, and 
+//  returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+//  Examples
+//  n = 0  ==> [1]        # [2^0]
+//  n = 1  ==> [1, 2]     # [2^0, 2^1]
+//  n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+function powersOfTwo(n){
+  let powers = []
+  
+  for (let i = 0; i <= n; i++) {
+    powers.push(Math.pow(2, i))
+  }
+  return powers
+}
+
+
 //  Level: 6 Kyu
 
 //  Unpack Delicious Sausages!
