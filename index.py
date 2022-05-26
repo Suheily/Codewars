6d37c4f0f899e85fc7b52c2a1ac88e71ec61ec0f
Suheily <suheilyq@gmail.com>
2022-05-26 23:50:23 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,23 @@
+#  Level: 8 Kyu
+
+#  Powers of 2
+#  Date: 5/26
+#  Instructions: Complete the function that takes a non-negative integer n as input, and 
+#  returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+#  Examples
+#  n = 0  ==> [1]        # [2^0]
+#  n = 1  ==> [1, 2]     # [2^0, 2^1]
+#  n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+import math
+def powers_of_two(n):
+    powers = []
+    
+    for i in range(0, n + 1):
+        powers.append(math.pow(2,i))
+    return powers
+
+
 #  Level: 6 Kyu
 
 #  Unpack Delicious Sausages!
