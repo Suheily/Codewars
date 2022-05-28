767cde2e4583e988b89b80da435687ed2f8ba3ff
Suheily <suheilyq@gmail.com>
2022-05-28 23:33:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//  Level: 4 Kyu
+
+//  Snail Sort
+//  Date: 5/28
+//  Instructions: Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+//  array = [[1,2,3],
+//         [4,5,6],
+//         [7,8,9]]
+//  snail(array) #=> [1,2,3,6,9,8,7,4,5]
+//  For better understanding, please follow the numbers of the next array consecutively:
+//  array = [[1,2,3],
+//          [8,9,4],
+//          [7,6,5]]
+//  snail(array) #=> [1,2,3,4,5,6,7,8,9]
+//  NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+//  NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+
+snail = function(array) {
+  let snailArray = []
+  
+  while(array.length > 0) {
+    snailArray = snailArray.concat(array.shift());
+    
+    array.forEach(function (current) {
+      snailArray.push(current.pop())
+      current.reverse()
+    })
+    
+    array.reverse()
+  }
+  
+  return snailArray
+}
+
+
 //  Level: 8 Kyu
 
 //  Powers of 2
