c7fb906f3ed2c9b7a4af9993a9f3fe06ea04124d
Suheily <suheilyq@gmail.com>
2022-05-28 22:44:53 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,43 @@
+#  Level: 4 Kyu
+
+#  Snail Sort
+#  Date: 5/28
+#  Instructions: Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+#  array = [[1,2,3],
+#          [4,5,6],
+#          [7,8,9]]
+#  snail(array) #=> [1,2,3,6,9,8,7,4,5]
+#  For better understanding, please follow the numbers of the next array consecutively:
+#  array = [[1,2,3],
+#          [8,9,4],
+#          [7,6,5]]
+#  snail(array) #=> [1,2,3,4,5,6,7,8,9]
+#  NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+#  NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+
+def snail(snail_map):
+    snail_array = []
+    
+    while len(snail_map) > 0:
+        snail_array += snail_map[0]
+        del snail_map[0]
+            
+        if len(snail_map) > 0:
+            for i in snail_map:
+                snail_array += [i[-1]]
+                del i[-1]
+                    
+            if snail_map[-1]:
+                snail_array += snail_map[-1][::-1]
+                del snail_map[-1]
+                    
+            for i in reversed(snail_map):
+                snail_array += [i[0]]
+                del i[0]
+                    
+    return snail_array
+
+
 #  Level: 8 Kyu
 
 #  Powers of 2
