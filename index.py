bbd15c52db9968d833755d390166baa72dbb6914
Suheily <suheilyq@gmail.com>
2022-05-30 00:31:23 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,29 @@
+#  Level: 5 Kyu
+
+#  Beeramid
+#  Date: 5/30
+#  Instructions: Let's pretend your company just hired your friend from college and paid you a referral bonus.
+#  Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral 
+#  bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those 
+#  beers, because let's pretend it's Friday too.
+#  A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in 
+#  the next, 16, 25...
+#  Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given 
+#  the parameters of: your referral bonus, and the price of a beer can
+#  For example:
+#  beeramid(1500, 2); // should === 12
+#  beeramid(5000, 3); // should === 16
+
+def beeramid(bonus, price):
+    levels = 0
+    cans_sum = int(bonus/price)
+    
+    while cans_sum >= (levels + 1)**2:
+        levels += 1
+        cans_sum -= levels * levels
+    return levels
+
+
 #  Level: 4 Kyu
 
 #  Snail Sort
