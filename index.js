a515b2a9c26cf7ba1e0e91f05cd66ba40d1ef708
Suheily <suheilyq@gmail.com>
2022-06-04 10:32:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 //  Level: 7 Kyu
 
+//  Find The Divisors!
+//  Date: 6/4
+//  Instructions: Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all 
+//  of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime 
+//  return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+//  Example:
+//  divisors(12); #should return [2,3,4,6]
+//  divisors(25); #should return [5]
+//  divisors(13); #should return "13 is prime"
+
+function divisors(integer) {
+  let divisors = []
+  let i = 1
+  
+  while(++i < integer){
+    (integer % i !== 0) || divisors.push(i)
+  }
+  return divisors.length <= 1? (integer + " is prime") : divisors
+}
+
+//  Level: 7 Kyu
+
 //  Money, Money, Money
 //  Date: 6/3
 //  Instructions: Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how 
