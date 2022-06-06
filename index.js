783eedf4d328f80413b17daea813f50fe654b443
Suheily <suheilyq@gmail.com>
2022-06-06 22:36:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+//  Level: 6 Kyu
+
+//  Sum of Digits / Digital Root
+//  Date: 6/6
+//  Instructions: Digital root is the recursive sum of all the digits in a number. Given n, take the sum of 
+//  the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit
+//  number is produced. The input will be a non-negative integer.
+//  Examples
+//  16  -->  1 + 6 = 7
+//  942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+//  132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+//  493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  --> 1 + 1 = 2
+
+function digital_root(n) {
+  let root = 0
+  
+  if(n < 10){
+    return n 
+  } else {
+    n = n.toString()
+    for(let i = 0; i < n.length; i++){
+    root += parseInt(n[i])
+  }
+    return digital_root(root)
+  }
+}
+
+
 //  Level: 7 Kyu
 
 //  Find The Divisors!
