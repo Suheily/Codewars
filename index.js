e3fb96c5d657e3b6750976a327708612fc169e63
Suheily <suheilyq@gmail.com>
2022-06-07 22:17:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//  Level: 5 Kyu
+
+//  Calculating with Functions
+//  Date: 6/7
+//  Instructions: This time we want to write calculations using functions and get the results. Let's have a 
+//  look at some examples:
+//  seven(times(five())); // must return 35
+//  four(plus(nine())); // must return 13
+//  eight(minus(three())); // must return 5
+//  six(dividedBy(two())); // must return 3
+//  Requirements:
+//  There must be a function for each number from 0 ("zero") to 9 ("nine")
+//  There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+//  Each calculation consist of exactly one operation and two numbers
+//  The most outer function represents the left operand, the most inner function represents the right operand
+//  Division should be integer division. For example, this should return 2, not 2.666666...:
+//  eight(dividedBy(three()));
+
+function zero(n) {return n? n(0):0 }
+function one(n) {return n? n(1):1}
+function two(n) {return n? n(2):2}
+function three(n) {return n? n(3):3}
+function four(n) {return n? n(4):4}
+function five(n) {return n? n(5):5}
+function six(n) {return n? n(6):6}
+function seven(n) {return n? n(7):7}
+function eight(n) {return n? n(8):8}
+function nine(n) {return n? n(9):9}
+
+function plus(b) { return function(a) {return a + b}}
+function minus(b) {return function(a) {return a - b}}
+function times(b) {return function(a) {return a * b}}
+function dividedBy(b) {return function(a) {return Math.floor(a / b)}}
+
+
 //  Level: 6 Kyu
 
 //  Sum of Digits / Digital Root
