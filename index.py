1ed83246d675b9823eaecda72698e84fa9390f52
Suheily <suheilyq@gmail.com>
2022-06-08 23:32:57 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,27 @@
+#  Level: 8 Kyu
+
+#  To Square(Root) Or Not To Square(Root)
+#  Date: 6/8
+#  Instructions: Write a method, that will get an integer array as parameter and will process every number 
+#  from this array. Return a new array with processing every number of the input-array like this:
+#  If the number has an integer square root, take this, otherwise square the number.
+#  Example
+#  [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+#  Notes: The input array will always contain only positive numbers, and will never be empty or null.
+
+import math
+
+def square_or_square_root(arr):
+    new_arr = []
+    
+    for i in arr:
+        if math.sqrt(i).is_integer():
+            new_arr.append(math.sqrt(i))
+        else:
+            new_arr.append(i**2)
+    return new_arr
+
+
 #  Level: 5 Kyu
 
 #  Calculating with Functions
