0c6f9c5c4cc4656b46708d2d83024f886fc6a0a5
Suheily <suheilyq@gmail.com>
2022-06-08 23:40:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+//  Level: 8 Kyu
+
+//  To Square(Root) Or Not To Square(Root)
+//  Date: 6/8
+//  Instructions: Write a method, that will get an integer array as parameter and will process every number 
+//  from this array. Return a new array with processing every number of the input-array like this:
+//  If the number has an integer square root, take this, otherwise square the number.
+//  Example
+//  [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+//  Notes: The input array will always contain only positive numbers, and will never be empty or null.
+
+function squareOrSquareRoot(array) {
+  let newArray = []
+  
+  for(let i = 0; i < array.length; i++){
+    if(Number.isInteger(Math.sqrt(array[i]))){
+      newArray.push(Math.sqrt(array[i]))
+    } else {
+      newArray.push(array[i]**2)
+    }
+  }
+  return newArray 
+}
+
+
 //  Level: 5 Kyu
 
 //  Calculating with Functions
