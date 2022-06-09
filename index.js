5fe8dfeb84b993fc2817703facbbea64e0915ad9
Suheily <suheilyq@gmail.com>
2022-06-09 19:35:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+//  Level: 5 Kyu
+
+//  A Chain Adding Function
+//  Date: 6/9
+//  Instructions: We want to create a function that will add numbers together when called in succession.
+//  add(1)(2); // == 3
+//  We also want to be able to continue to add numbers to our chain.
+//  add(1)(2)(3); // == 6
+//  add(1)(2)(3)(4); //  == 10
+//  add(1)(2)(3)(4)(5); // == 15
+//  and so on.
+//  A single call should be equal to the number passed in.
+//  add(1); // == 1
+//  We should be able to store the returned values and reuse them.
+//  var addTwo = add(2);
+//  addTwo; // == 2
+//  addTwo + 5; // == 7
+//  addTwo(3); // == 5
+//  addTwo(3)(5); // == 10
+//  We can assume any number being passed in will be valid whole number.
+
+function add(n){
+  var fn = function(i) {
+    
+  return add(n + i)
+  }
+  fn.valueOf = function() {
+    return n
+  }
+  return fn
+}
+
+
 //  Level: 8 Kyu
 
 //  To Square(Root) Or Not To Square(Root)
