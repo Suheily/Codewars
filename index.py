736b430881ed01dd0f0cf5e861a83635449bec3b
Suheily <suheilyq@gmail.com>
2022-06-11 13:38:26 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,33 @@
 #  Level: 8 Kyu
 
+#  How Many Stairs Will Suzuki Climb in 20 Years?
+#  Date: 6/11
+#  Instructions: Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. Some
+#  days he climbs more stairs than others depending on the number of students he must train in the morning.
+#  He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down 
+#  his daily progress. The sum of all the stairs logged in a year will be used for estimating the number he
+#  might climb in 20.
+#  20_year_estimate = one_year_total * 20
+#  You will receive the following data structure representing the stairs Suzuki logged in a year. You will 
+#  have all data for the entire year so regardless of how it is logged the problem should be simple to solve.
+#  stairs = [sunday,monday,tuesday,wednesday,thursday,friday,saturday]
+#  Make sure your solution takes into account all of the nesting within the stairs array.
+#  Each weekday in the stairs array is an array.
+#  sunday = [6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543, 5153, 6832, 8274, 7148, 6152, 5940, 8040, 
+#  9174, 7555, 7682, 5252, 8793, 8837, 7320, 8478, 6063, 5751, 9716, 5085, 7315, 7859, 6628, 5425, 6331, 7097,
+#  6249, 8381, 5936, 8496, 6934, 8347, 7036, 6421, 6510, 5821, 8602, 5312, 7836, 8032, 9871, 5990, 6309, 7825]
+#  Your function should return the 20 year estimate of the stairs climbed using the formula above.
+
+def stairs_in_20(stairs):
+    estimate = 0
+    for day in stairs:
+        for steps in day:
+            estimate += steps
+    return estimate * 20
+
+
+#  Level: 8 Kyu
+
 #  Welcome to the City
 #  Date: 6/10
 #  Instructions: Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state
@@ -41,7 +69,7 @@ class add(int):
     
 #  Level: 8 Kyu
 
-#  To Square(Root) Or Not To Square(Root)
+#  To Square(Root) Or Not to Square(Root)
 #  Date: 6/8
 #  Instructions: Write a method, that will get an integer array as parameter and will process every number 
 #  from this array. Return a new array with processing every number of the input-array like this:
