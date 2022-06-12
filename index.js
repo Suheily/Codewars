8d805ec4bd237e8cf907ea49de984fad4775e087
Suheily <suheilyq@gmail.com>
2022-06-12 23:52:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
+//  Level: 5 Kyu
+
+//  Tic-Tac-Toe Checker
+//  Date: 6/12
+//  Instructions: If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state
+//  is solved, wouldn't we? Our goal is to create a function that will check that for us! Assume that the board 
+//  comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is
+//  an "O", like so:
+//  [[0, 0, 1],
+//   [0, 1, 2],
+//   [2, 1, 0]]
+//  We want our function to return:
+//  -1 if the board is not yet finished AND no one has won yet (there are empty spots),
+//  1 if "X" won,
+//  2 if "O" won,
+//  0 if it's a cat's game (i.e. a draw).
+//  You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+
+function isSolved(board) {
+  
+  function checkRow() {
+  let result
+  
+  board.forEach(function(choice){
+    if (choice[0] == choice[1] &&
+        choice[0] == choice[2] &&
+        choice[1] == choice[2] &&
+        choice[0] != 0){
+      result = choice[0]
+    }
+  })
+    return result
+  }
+  
+  function checkCol() {
+    for (let i = 0; i < 3; i++){
+      if (board[0][i]==board[1][i]&&
+          board[1][i]==board[2][i]&&
+          board[2][i]==board[0][i]&&
+          board[0][i]!=0){
+        return board[0][i]
+      }  
+    }
+  }
+  
+  function checkDiagonal(){
+    if ((board[0][0]==board[1][1]&&
+         board[1][1]==board[2][2]&&
+         board[2][2]==board[0][0])||
+        (board[0][2]==board[1][1]&&
+         board[1][1]==board[2][0]&&
+         board[2][0]==board[0][2])){
+      return board[1][1]
+      }
+  }  
+ 
+  function gameOver() {
+    for (let x = 0; x < 3; x++){
+      for (let y = 0; y < 3; y++){
+        if  (board[x][y] == 0){
+          return -1
+        }
+      }
+    }
+  return 0
+  }
+  
+  return checkRow() || checkCol() || checkDiagonal() || gameOver()
+}
+
+
 //   Level: 8 Kyu
 
 //   How Many Stairs Will Suzuki Climb in 20 Years?
