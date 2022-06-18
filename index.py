5fe20b56d58ec706284ca93a7b4c484254de12cf
Suheily <suheilyq@gmail.com>
2022-06-18 22:14:57 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,17 @@
+#  Level: 8 Kyu
+
+#  Convert a Number to a String!
+#  Date: 6/18
+#  Instructions: We need a function that can transform a number into a string.
+#  What ways of achieving this do you know?
+#  Examples:
+#  123 --> "123"
+#  999 --> "999" 
+
+def number_to_string(num):
+    return str(num)
+
+
 #  Level: 7 Kyu
 
 #  Creating a Bitset, Part 1
