b36524a4a3c54d7032d16a6e9e088a5baf82cc15
Suheily <suheilyq@gmail.com>
2022-06-18 22:10:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+//  Level: 8 Kyu
+
+//  Convert a Number to a String!
+//  Date: 6/18
+//  Instructions: We need a function that can transform a number into a string.
+//  What ways of achieving this do you know?
+//  Examples:
+//  123 --> "123"
+//  999 --> "999"
+
+function numberToString(num) {
+  return num.toString()
+}
+
+
 //  Level: 7 Kyu
 
 //  Creating a Bitset, Part 1
