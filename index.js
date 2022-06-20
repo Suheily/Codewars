4f378cd9ee52cc3e17a9cc6d174b6957ad1da30f
Suheily <suheilyq@gmail.com>
2022-06-20 23:02:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//  Level: 7 Kyu
+
+//  Filtering Lists
+//  Date: 6/20
+//  Instructions: In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list 
+//  with the strings filtered out.
+//  Examples:
+//  filter_list([1,2,'a','b']) == [1,2]
+//  filter_list([1,'a','b',0,15]) == [1,0,15]
+//  filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+  return l.filter(function(i) {return typeof i != "string"})
+}
+
+
 //  Level: 8 Kyu
 
 //  Convert a Number to a String!
