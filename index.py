8752407273803fb07859ca891746c8d0baa9c64b
Suheily <suheilyq@gmail.com>
2022-06-20 22:52:26 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
+#  Level: 7 Kyu
+
+#  Filtering Lists
+#  Date: 6/20
+#  Instructions: In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list 
+#  with the strings filtered out.
+#  Examples:
+#  filter_list([1,2,'a','b']) == [1,2]
+#  filter_list([1,'a','b',0,15]) == [1,0,15]
+#  filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+def filter_list(l):
+    return [i for i in l if not isinstance(i, str)]
+
+
 #  Level: 8 Kyu
 
 #  Convert a Number to a String!
 #  Date: 6/18
-#  Instructions: We need a function that can transform a number into a string.
-#  What ways of achieving this do you know?
+#  Instructions: We need a function that can transform a number into a string. What ways of achieving this do you know?
 #  Examples:
 #  123 --> "123"
 #  999 --> "999" 
