bd2c228ff4a8d24ed7f2c01fb1f8072256e11daa
Suheily <suheilyq@gmail.com>
2022-06-23 20:47:29 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,19 @@
+#  Level: 8 Kyu
+
+#  Regular Ball Super Ball
+#  Date: 6/23
+#  Instructions: Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+#  If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+#  ball1 = new Ball();
+#  ball2 = new Ball("super");
+#  ball1.ballType     //=> "regular"
+#  ball2.ballType     //=> "super"
+
+class Ball(object):
+    def __init__(self, ball_type = "regular"):
+        self.ball_type = ball_type
+
+
 #  Level: 6 Kyu
 
 #  What's Your Running Pace?
