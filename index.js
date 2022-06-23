990b957beef261ed03f1bcae9d398f337cec612d
Suheily <suheilyq@gmail.com>
2022-06-23 20:45:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+//  Level: 8 Kyu
+
+//  Regular Ball Super Ball
+//  Date: 6/23
+//  Instructions: Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+//  If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+//  ball1 = new Ball();
+//  ball2 = new Ball("super");
+//  ball1.ballType     //=> "regular"
+//  ball2.ballType     //=> "super"
+
+class Ball {
+  constructor(ballType = "regular") {
+    this.ballType = ballType
+  }
+}
+
+
 //  Level: 6 Kyu
 
 //  What's Your Running Pace?
