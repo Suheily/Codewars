ec7b729190329c654f08cb19ee910005624d2c7c
Suheily <suheilyq@gmail.com>
2022-06-27 23:53:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 //  Level: 7 Kyu
 
+//  Baby Shark Lyrics Generator
+//  Date: 6/27
+//  Instructions: Create a function, as short as possible, that returns these lyrics. Your code should be less than 300 characters.
+//  Watch out for the three points at the end of the song.
+//  Baby shark, doo doo doo doo doo doo
+//  Baby shark, doo doo doo doo doo doo
+//  Baby shark, doo doo doo doo doo doo
+//  Baby shark!
+//  Mommy shark, doo doo doo doo doo doo
+//  Mommy shark, doo doo doo doo doo doo
+//  Mommy shark, doo doo doo doo doo doo
+//  Mommy shark!
+//  Daddy shark, doo doo doo doo doo doo
+//  Daddy shark, doo doo doo doo doo doo
+//  Daddy shark, doo doo doo doo doo doo
+//  Daddy shark!
+//  Grandma shark, doo doo doo doo doo doo
+//  Grandma shark, doo doo doo doo doo doo
+//  Grandma shark, doo doo doo doo doo doo
+//  Grandma shark!
+//  Grandpa shark, doo doo doo doo doo doo
+//  Grandpa shark, doo doo doo doo doo doo
+//  Grandpa shark, doo doo doo doo doo doo
+//  Grandpa shark!
+//  Let's go hunt, doo doo doo doo doo doo
+//  Let's go hunt, doo doo doo doo doo doo
+//  Let's go hunt, doo doo doo doo doo doo
+//  Let's go hunt!
+//  Run away,…
+
+function babySharkLyrics(){
+  names = ["Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt"];
+  return names.map((name)=> `${name},${" doo doo doo doo doo doo"}\n`.repeat(3) + `${name}!\n`).join("") + "Run away,…";
+}
+
+
+//  Level: 7 Kyu
+
 //  Truthy and Falsy
 //  Date: 6/26
 //  Instructions: In JavaScript there are truthy and falsy values. You are given two empty arrays (truthy and falsy) and you have to 
