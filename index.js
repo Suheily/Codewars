974da907fcb04a4c135012e0ec5ef618ac3178b8
Suheily <suheilyq@gmail.com>
2022-06-30 23:15:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+//  Level: 8 Kyu
+
+//  Rock, Paper, Scissors!
+//  Date: 6/30
+//  Instructions: Let's play! You have to return which player won! In case of a draw return Draw!.
+//  Examples(Input1, Input2 --> Output):
+//  "scissors", "paper" --> "Player 1 won!"
+//  "scissors", "rock" --> "Player 2 won!"
+//  "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+  if (p1 == "scissors" && p2 == "paper"){
+    return "Player 1 won!"
+  } else if (p1 == "scissors" && p2 == "rock") {
+    return "Player 2 won!"
+  } else if (p1 == "rock" && p2 == "scissors") {
+    return "Player 1 won!"
+  } else if (p1 == "rock" && p2 == "paper") {
+    return "Player 2 won!"
+  } else if (p1 == "paper" && p2 == "rock") {
+    return "Player 1 won!"
+  } else if (p1 == "paper" && p2 == "scissors") {
+    return "Player 2 won!"
+  } else if (p1 == p2) {
+    return "Draw!"
+  }
+}
+
+
+//  Level: 7 Kyu
+
 //  You're A Square
 //  Date: 6/29
 //  Instructions: A square of squares. You like building blocks. You especially like building blocks that are squares. And what you even
