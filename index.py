966ad5413e7f8ce31a3028672f10893db4b7c6b9
Suheily <suheilyq@gmail.com>
2022-07-01 00:21:10 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,30 @@
+#  Level: 8 Kyu
+
+#  Rock, Paper, Scissors!
+#  Date: 6/30
+#  Instructions: Let's play! You have to return which player won! In case of a draw return Draw!.
+#  Examples(Input1, Input2 --> Output):
+#  "scissors", "paper" --> "Player 1 won!"
+#  "scissors", "rock" --> "Player 2 won!"
+#  "paper", "paper" --> "Draw!"
+
+def rps(p1, p2):
+    if p1 == "scissors" and p2 == "paper":
+        return "Player 1 won!"
+    elif p1 == "scissors" and p2 == "rock":
+        return "Player 2 won!"
+    elif p1 == "rock" and p2 == "scissors":
+        return "Player 1 won!"
+    elif p1 == "rock" and p2 == "paper":
+        return "Player 2 won!"
+    elif p1 == "paper" and p2 == "rock":
+        return "Player 1 won!"
+    elif p1 == "paper" and p2 == "scissors":
+        return "Player 2 won!"
+    elif p1 == p2:
+        return "Draw!"
+
+
 #  Level: 7 Kyu
 
 #  You're A Square
