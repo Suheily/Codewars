16696801f92565b0ae6a3ef0899d486f4155331a
Suheily <suheilyq@gmail.com>
2022-07-06 18:59:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 //  Level: 8 Kyu
 
+//  Merge Two Sorted Arrays into One
+//  Date: 7/6
+//  Instructions: You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted 
+//  in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays. You don't need to worry about 
+//  validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+//  Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+//  Examples (input -> output)
+//  * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+
+
+function mergeArrays(arr1, arr2) {
+  return [...new Set(arr1.concat(arr2))].sort((a,b) => a - b)
+}
+
+
+//  Level: 8 Kyu
+
 //  L1: Set Alarm
 //  Date: 7/4
 //  Instructions: Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed 
