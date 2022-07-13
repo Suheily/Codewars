1d46b0948c86ab4331caba304a452f43c1a6994f
Suheily <suheilyq@gmail.com>
2022-07-13 20:10:37 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 #  Level: 8 Kyu
 
+#  Is This My Tail?
+#  Date: 7/13
+#  Instructions: Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help
+#  her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body)
+#  - otherwise the tail wouldn't fit! If the tail is right return true, else return false. The arguments will always be non empty strings, and normal letters.
+
+def correct_tail(body, tail):
+     return body.endswith(tail)
+
+
+#  Level: 8 Kyu
+
 #  Playing with Cubes II
 #  Date: 7/10
 #  Instructions: Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of
