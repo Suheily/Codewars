e0e7bbfd6809a47ef6cb0dcecad6ce51dbd02102
Suheily <suheilyq@gmail.com>
2022-07-14 14:10:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 //  Level: 8 Kyu
 
+//  Is This My Tail?
+//  Date: 7/14
+//  Instructions: Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help
+//  her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body)
+//  - otherwise the tail wouldn't fit! If the tail is right return true, else return false. The arguments will always be non empty strings, and normal letters.
+
+function correctTail(body, tail){  
+  let sub = body.substr(body.length-(tail.length))
+  if (sub == tail){
+    return true
+  } else {
+    return false
+  }
+}
+
+
+//  Level: 8 Kyu
+
 //  Playing with Cubes II
 //  Date: 7/9
 //  Instructions: Hey Codewarrior! You already implemented a Cube class, but now we need your help again! I'm talking about constructors. 
