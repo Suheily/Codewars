264f97ad0c4a274243e94d37c6863b38e07b599e
Suheily <suheilyq@gmail.com>
2022-07-15 13:05:14 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,25 @@
+#  Level: 5 Kyu
+
+#  Moving Zeros To The End
+#  Date: 7/15
+#  Instructions: Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+#  move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+
+def move_zeros(lst):
+    counter = 0
+    new_lst = []
+     
+    for i in lst:
+        if i == 0: 
+            counter +=1
+        else:
+            new_lst.append(i)
+    while counter > 0:
+        new_lst.append(0)
+        counter -= 1
+    return new_lst
+
+
 #  Level: 8 Kyu
 
 #  Is This My Tail?
