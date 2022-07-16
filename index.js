2eb08543efc1a16aa18636e61115199779476035
Suheily <suheilyq@gmail.com>
2022-07-17 01:08:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+//  Level: 5 Kyu
+
+//  Moving Zeros To The End
+//  Date: 7/17
+//  Instructions: Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+//  move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+
+function moveZeros(arr) {
+  let counter = 0
+  let newList = []
+
+  for(let i = 0; i < arr.length; i++){
+    if(typeof arr[i] == "number" && arr[i] == 0){
+      counter += 1
+    } else {
+      newList.push(arr[i])
+    }
+  } 
+  while(counter > 0){
+    newList.push(0)
+    counter -= 1
+  } 
+  return newList
+}
+
+
 //  Level: 8 Kyu
 
 //  Find The Position!
