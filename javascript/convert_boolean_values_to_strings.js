6b07838cc398377fc5abe3fbf320c05e6b118265
Suheily <suheilyq@gmail.com>
2022-07-21 00:41:05 +0900

    javascript convert boolean values to strings

---
+++ javascript/convert_boolean_values_to_strings.js
@@ -0,0 +1,13 @@
+//  Level: 8 Kyu
+
+//  Convert Boolean Values to Strings 'Yes' or 'No'
+//  Date: 7/21
+//  Instructions: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  if(bool == true){
+    return "Yes"
+  } else {
+    return "No"
+  }
+}
