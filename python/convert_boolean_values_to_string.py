92fb78d375c4b7c997a8a412b977573da890f30d
Suheily <suheilyq@gmail.com>
2022-07-24 23:41:58 +0900

    python convert boolean values to string

---
+++ python/convert_boolean_values_to_string.py
@@ -0,0 +1,8 @@
+#  Level: 8 Kyu
+
+#  Convert Boolean Values to Strings 'Yes' or 'No'
+#  Date: 7/21
+#  Instructions: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
