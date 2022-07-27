57a2eaaf4dfee3b40c4c2e6f89c9b69844e6b8a6
Suheily <suheilyq@gmail.com>
2022-07-27 17:06:03 +0900

    update quarter of the year

--- javascript/quarter_of_the_year.js
+++ javascript/quarter_of_the_year.js
@@ -6,13 +6,5 @@
 // Examples: Month 2 (February) is part of the first quarter, month 6 (June) is part of the second quarter, and month 11 (November) is part of the fourth quarter.
 
 const quarterOf = (month) => {
-  if(month <= 3){
-    return 1
-  } else if (month <= 6){
-    return 2
-  } else if (month <= 9){
-    return 3
-  } else if (month <= 12){
-    return 4
-  }
+  return Math.ceil(month / 3)
 }
--- python/quarter_of_the_year.py
+++ python/quarter_of_the_year.py
@@ -4,13 +4,8 @@
 #  Date: 7/25
 #  Instructions: Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 #  Examples: Month 2 (February) is part of the first quarter, month 6 (June) is part of the second quarter, and month 11 (November) is part of the fourth quarter.
+import math
+
 
 def quarter_of(month):
-    if month <= 3:
-        return 1
-    elif month <= 6:
-        return 2
-    elif month <= 9:
-        return 3
-    elif month <= 12:
-        return 4
+    return math.ceil(month / 3)
