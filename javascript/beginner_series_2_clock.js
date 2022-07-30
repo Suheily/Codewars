8cc94055c215d4a32221694a996a8dda9d62a1fa
Suheily <suheilyq@gmail.com>
2022-07-30 21:39:46 +0900

    javascript beginner series 2 clock

---
+++ javascript/beginner_series_2_clock.js
@@ -0,0 +1,15 @@
+// Level: 8 Kyu
+
+// Beginner Series #2 Clock
+// Date: 7/30
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+// Example:
+// h = 0
+// m = 1
+// s = 1
+// result = 61000
+
+function past(h, m, s){
+  return ((h * 3600) + (m * 60) + s ) * 1000
+}
