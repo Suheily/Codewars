8e2285810bf59c3464ddf988554ada4113c1e574
Suheily <suheilyq@gmail.com>
2022-08-25 00:00:37 +0900

    javascript string ends with

---
+++ javascript/string_ends_with.js
@@ -0,0 +1,16 @@
+// Level: 7 Kyu
+
+// String Ends With?
+// Date: 8/25
+// Instructions: Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// Examples:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+  if(str.endsWith(ending)){
+    return true
+  } else {
+    return false
+  }
+}
