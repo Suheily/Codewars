f9c58c75d0037a3c62125248899f15c3bf00e4ab
Suheily <suheilyq@gmail.com>
2022-08-31 23:59:14 +0900

    javascript remove string spaces

---
+++ javascript/remove_string_spaces.js
@@ -0,0 +1,9 @@
+//  Level: 8 Kyu
+
+//  Remove String Spaces
+//  Date: 8/31
+//  Instructions: Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+    return x.split(" ").join("")
+  }
