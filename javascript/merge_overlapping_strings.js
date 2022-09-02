e646442c543b370f65a25b77385430d6b7e0b656
Suheily <suheilyq@gmail.com>
2022-09-02 22:15:55 +0900

    javascript merge overlapping strings

---
+++ javascript/merge_overlapping_strings.js
@@ -0,0 +1,20 @@
+// Level: 7 Kyu
+
+// Merge Overlapping Strings
+// Date: 9/2
+// Instructions: This kata requires you to write a function which merges two strings together. It does so by merging the end of the first string with the start of the second string together when they are an exact match.
+// "abcde" + "cdefgh" => "abcdefgh"
+// "abaab" + "aabab" => "abaabab"
+// "abc" + "def" => "abcdef"
+// "abc" + "abc" => "abc"
+// NOTE: The algorithm should always use the longest possible overlap.
+// "abaabaab" + "aabaabab" would be "abaabaabab" and not "abaabaabaabab"
+
+function mergeStrings(first, second) {
+  for (let i = 0; i < first.length; i++){
+    if (second.startsWith(first.substring(i))){
+      return first + second.substring(first.length - i);
+      }
+    }
+  return first + second;
+}
