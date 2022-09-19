2de55fdfdca9f810cdf4aa4a3e222916bd93e3de
Suheily <suheilyq@gmail.com>
2022-09-19 22:40:59 +0900

    javascript function 1 hello world

---
+++ javascript/function_1.js
@@ -0,0 +1,7 @@
+// Level: 8 Kyu
+
+// Function 1 - Hello World!
+// Date: 9/19
+// Instructions: Make a simple function called greet that returns the most-famous "hello world!".
+
+const greet = () => "hello world!";
