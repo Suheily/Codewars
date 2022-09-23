1f89d3bdc2dc35127f073963524fcc722b82255f
Suheily <suheilyq@gmail.com>
2022-09-23 16:27:47 +0900

    javascript remove the minimum

---
+++ javascript/remove_minimum.js
@@ -0,0 +1,25 @@
+// Level: 7 Kyu
+
+// Remove The Minimum
+// Date: 9/23
+// Instructions: The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+// Don't change the order of the elements that are left.
+// Examples:
+// Input: [1,2,3,4,5], output= [2,3,4,5]
+// Input: [5,3,2,1,4], output = [5,3,2,4]
+// Input: [2,2,1,2,1], output = [2,2,2,1]
+
+function removeSmallest(numbers) {
+  let min = Infinity
+  let minIndex = 0
+  
+  for(let i = 0;i < numbers.length; i++){
+      if(numbers[i] < min){
+        min = numbers[i]
+        minIndex = i
+      }
+    }
+  return numbers.slice(0, minIndex).concat(numbers.slice(minIndex + 1));
+}
