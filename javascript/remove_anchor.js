1671c4306ea09409a0b85b331521c291cabbed62
Suheily <suheilyq@gmail.com>
2022-10-04 21:50:46 +0900

    javascript remove anchor from url

---
+++ javascript/remove_anchor.js
@@ -0,0 +1,12 @@
+// Level: 7 Kyu
+
+// Remove Anchor From URL
+// Date: 10/4
+// Instructions: Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+// Examples:
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+  return url.split("#")[0]
+}
